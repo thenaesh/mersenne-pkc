3ce279ba12229dd7b47bfb467128bbcbfa357186
Thenaesh Elango <thenaeshelango@gmail.com>
2019-02-18 22:14:10 +0800

    Add 2-pass decryption algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,12 @@ pub mod bit_field;
 use std::iter::Iterator;
 
 use crate::bit_field::BitField;
-use crate::finite_ring::FiniteRing;
 
 pub type PublicKey = BitField;
 pub type PrivateKey = (BitField, BitField);
 
 pub type PlainText = (BitField, BitField);
 pub type CipherText = BitField;
-
-const N_WORKERS: usize = 2;
-const N_JOBS: usize = 2;
 
 pub fn randomly_generate_message(n: usize, h: usize) -> PlainText {
     let a = BitField::new_uniform_random(n, h);
@@ -30,7 +26,7 @@ pub fn encrypt(m: PlainText, pub_key: PublicKey, h: usize) -> CipherText {
     c
 }
 
-pub fn decrypt(c: CipherText, pri_key: PrivateKey, h: usize) -> PlainText {
+pub fn decrypt_onepass(c: CipherText, pri_key: PrivateKey, h: usize) -> PlainText {
     let n = c.len();
 
     let (mut f, mut g) = pri_key;
@@ -60,19 +56,73 @@ pub fn decrypt(c: CipherText, pri_key: PrivateKey, h: usize) -> PlainText {
     (a, b)
 }
 
-// computes the list [x - y * 2^i for i in 0..n]
+// currently does 2 passes
+pub fn decrypt(c: CipherText, pri_key: PrivateKey, h: usize) -> PlainText {
+    let n = c.len();
+
+    let (mut f, mut g) = pri_key;
+    f.make_sparse();
+    g.make_sparse();
+
+    let mut z = c.clone();
+    z *= &g;
+    let mut z = z.extend(1);
+    z.make_sparse();
+    z.set(n);
+    z.normalize();
+
+    let potential_a_bits = get_hamming_weight_changes_on_subtraction(&z, &f);
+    let potential_b_bits = get_hamming_weight_changes_on_subtraction(&z, &g);
+
+    let mut a = BitField::new_dense(n);
+    let mut b = BitField::new_dense(n);
+
+    for idx in 0..h/2 {
+        let (i, _) = potential_a_bits[idx];
+        let (j, _) = potential_b_bits[idx];
+        a.set(i);
+        b.set(j);
+    }
+
+    let mut tmp_af = a.clone();
+    let mut tmp_bg = b.clone();
+    tmp_af *= &f;
+    tmp_bg *= &g;
+
+    let mut z = c.clone();
+    z *= &g;
+    z -= &tmp_af;
+    z -= &tmp_bg;
+    let mut z = z.extend(1);
+    z.make_sparse();
+    z.set(n);
+    z.normalize();
+
+    let potential_a_bits = get_hamming_weight_changes_on_subtraction(&z, &f);
+    let potential_b_bits = get_hamming_weight_changes_on_subtraction(&z, &g);
+
+    for idx in 0..h/2 {
+        let (i, _) = potential_a_bits[idx];
+        let (j, _) = potential_b_bits[idx];
+        a.set(i);
+        b.set(j);
+    }
+
+    (a, b)
+}
+
 pub fn get_hamming_weight_changes_on_subtraction(minuend: &BitField, subtrahend: &BitField) -> Vec<(usize, i64)> {
     let n = minuend.len();
-    let m = subtrahend.len();
 
     let mut result: Vec<(usize, i64)> = (0..n).map(|i| {
         let mut subtrahend = subtrahend.clone();
         subtrahend <<= i;
         subtrahend.normalize();
-        let mut subtrahend = subtrahend.extend(1);
+        let subtrahend = subtrahend.extend(1);
         let hamming_weight_change = minuend.hamming_weight_change_upon_subtraction(subtrahend);
         (i, hamming_weight_change)
     }).collect();
     result.sort_unstable_by_key(|(i, hwc)| -hwc);
+
     result
 }
