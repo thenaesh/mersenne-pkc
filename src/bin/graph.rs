dcb399ed4ef5d9e162c9842f40762f9e4a6c659e
Thenaesh Elango <thenaeshelango@gmail.com>
2019-02-18 19:47:13 +0800

    Add more statistics

--- src/bin/graph.rs
+++ src/bin/graph.rs
@@ -15,17 +15,18 @@ use plotlib::view::View;
 use plotlib::page::Page;
 
 fn main() {
-    let ns = vec![44497];
-    let hs = vec![64, 128];
+    let ns = vec![86243];
+    let hs = vec![128];
 
     let start_time = SystemTime::now();
 
     for h in hs.iter() {
         for n in ns.iter() {
             println!("n = {}, h = {}", *n, *h);
-            let (ex_mean, ex_var, unex_max_mean, unex_max_var, ex_max_mean, ex_max_var) = generate_and_plot_frequencies(*n, *h);
+            let (ex_mean, ex_var, unex_max_mean, unex_max_var, ex_min_mean, ex_min_var, ex_max_mean, ex_max_var) = generate_and_plot_frequencies(*n, *h);
             println!("Expected Points: mean = {}, stddev = {}", ex_mean, ex_var.sqrt());
             println!("Unexpected Max Points: mean = {}, stddev = {}", unex_max_mean, unex_max_var.sqrt());
+            println!("Expected Min Points: mean = {}, stddev = {}", ex_min_mean, ex_min_var.sqrt());
             println!("Expected Max Points: mean = {}, stddev = {}", ex_max_mean, ex_max_var.sqrt());
         }
     }
@@ -33,15 +34,21 @@ fn main() {
     println!("Elapsed Time: {}s", start_time.elapsed().unwrap().as_secs());
 }
 
-fn generate_and_plot_frequencies(n: usize, h: usize) -> (f64, f64, f64, f64, f64, f64) {
+fn generate_and_plot_frequencies(n: usize, h: usize) -> (f64, f64, f64, f64, f64, f64, f64, f64) {
     let mut expected_frequency_map = HashMap::<i64, usize>::new();
     let mut unexpected_max_frequency_map = HashMap::<i64, usize>::new();
+    let mut expected_min_frequency_map = HashMap::<i64, usize>::new();
     let mut expected_max_frequency_map = HashMap::<i64, usize>::new();
 
-    for i in 0..10000 {
-        let (xs, y, z) = generate_reductions(n, h);
+    for i in 0..5000 {
+        let (xs, y, w, z) = generate_reductions(n, h);
 
         unexpected_max_frequency_map.insert(y, match unexpected_max_frequency_map.get(&y) {
+            Some(a) => a + 1,
+            None => 1,
+        });
+
+        expected_min_frequency_map.insert(w, match expected_min_frequency_map.get(&z) {
             Some(a) => a + 1,
             None => 1,
         });
@@ -65,14 +72,16 @@ fn generate_and_plot_frequencies(n: usize, h: usize) -> (f64, f64, f64, f64, f64
 
     let expected_points: Vec<(f64, f64)> = expected_frequency_map.iter().map(|(hw, freq)| (*hw as f64, *freq as f64)).collect();
     let unexpected_max_points: Vec<(f64, f64)> = unexpected_max_frequency_map.iter().map(|(hw, freq)| (*hw as f64, *freq as f64)).collect();
+    let expected_min_points: Vec<(f64, f64)> = expected_min_frequency_map.iter().map(|(hw, freq)| (*hw as f64, *freq as f64)).collect();
     let expected_max_points: Vec<(f64, f64)> = expected_max_frequency_map.iter().map(|(hw, freq)| (*hw as f64, *freq as f64)).collect();
 
-    println!("{} expected points, {} unexpected max points, {} expected max points",
+    println!("{} expected points, {} unexpected max points, {} expected min points, {} expected max points",
         expected_points.iter().fold(0., |acc, (_, n)| acc + n),
         unexpected_max_points.iter().fold(0., |acc, (_, n)| acc + n),
+        expected_min_points.iter().fold(0., |acc, (_, n)| acc + n),
         expected_max_points.iter().fold(0., |acc, (_, n)| acc + n));
 
-    plot_frequencies(&format!("graphs/freq_plot_{}_{}.svg", n.to_string(), h.to_string()), &expected_points, &unexpected_max_points, &expected_max_points);
+    plot_frequencies(&format!("graphs/freq_plot_{}_{}.svg", n.to_string(), h.to_string()), &expected_points, &unexpected_max_points, &expected_min_points, &expected_max_points);
 
     let expected_points_sample_mean = {
         let (sum, n) = expected_points.iter().fold((0., 0.), |(sum, n), (hw, freq)| (sum + *hw * *freq, n + *freq));
@@ -98,6 +107,18 @@ fn generate_and_plot_frequencies(n: usize, h: usize) -> (f64, f64, f64, f64, f64
         var / (n - 1.)
     };
 
+    let expected_min_points_sample_mean = {
+        let (sum, n) = expected_min_points.iter().fold((0., 0.), |(sum, n), (hw, freq)| (sum + *hw * *freq, n + *freq));
+        sum / n
+    };
+    let expected_min_points_sample_variance = {
+        let (var, n) = expected_min_points.iter().fold((0., 0.), |(sumsq, n), (hw, freq)| {
+            let delta = *hw - expected_min_points_sample_mean;
+            (sumsq + freq * delta * delta, n + freq)
+        });
+        var / (n - 1.)
+    };
+
     let expected_max_points_sample_mean = {
         let (sum, n) = expected_max_points.iter().fold((0., 0.), |(sum, n), (hw, freq)| (sum + *hw * *freq, n + *freq));
         sum / n
@@ -112,6 +133,7 @@ fn generate_and_plot_frequencies(n: usize, h: usize) -> (f64, f64, f64, f64, f64
 
     (expected_points_sample_mean, expected_points_sample_variance,
         unexpected_max_points_sample_mean, unexpected_max_points_sample_variance,
+        expected_min_points_sample_mean, expected_min_points_sample_variance,
         expected_max_points_sample_mean, expected_max_points_sample_variance)
 }
 
@@ -145,7 +167,7 @@ fn generate_and_plot_points(n: usize, h: usize) {
     println!("Elapsed Time: {}s", start_time.elapsed().unwrap().as_secs());
 }
 
-fn plot_frequencies(output_filename: &str, expected_points: &Vec<(f64, f64)>, unexpected_max_points: &Vec<(f64, f64)>, expected_max_points: &Vec<(f64, f64)>) {
+fn plot_frequencies(output_filename: &str, expected_points: &Vec<(f64, f64)>, unexpected_max_points: &Vec<(f64, f64)>, expected_min_points: &Vec<(f64, f64)>, expected_max_points: &Vec<(f64, f64)>) {
     println!("Plotting Frequency Graph: {}", output_filename);
 
 
@@ -157,35 +179,38 @@ fn plot_frequencies(output_filename: &str, expected_points: &Vec<(f64, f64)>, un
 
     let (max_hw, max_freq) = (expected_max_hw.max(unexpected_max_hw), expected_max_freq.max(unexpected_max_freq));
 
-    println!("{:?}", expected_points);
-    println!("{:?}", unexpected_max_points);
-    println!("{:?}", expected_max_points);
+    println!("Expected: {:?}", expected_points);
+    println!("Unexpected: {:?}", unexpected_max_points);
+    println!("Expected Min: {:?}", expected_min_points);
+    println!("Expected Max: {:?}", expected_max_points);
 
-    // We create our scatter plot from the data
-    let s1 = Scatter::from_vec(expected_points.as_slice())
+    let s1 = Scatter::from_vec(unexpected_max_points.as_slice())
         .style(scatter::Style::new()
-            .marker(Marker::Square) // setting the marker to be a square
-            .colour("#00FF00")); // and a custom colour
+            .marker(Marker::Square)
+            .colour("#222222"));
 
-    let s2 = Scatter::from_vec(unexpected_max_points.as_slice())
-        .style(scatter::Style::new() // uses the default marker
-            .colour("#FF0000")); // and a different colour
+    let s2 = Scatter::from_vec(expected_points.as_slice())
+        .style(scatter::Style::new()
+            .colour("#0000FF"));
 
-    let s3 = Scatter::from_vec(expected_max_points.as_slice())
-        .style(scatter::Style::new() // uses the default marker
-            .colour("#0000FF")); // and a different colour
+    let s3 = Scatter::from_vec(expected_min_points.as_slice())
+        .style(scatter::Style::new()
+            .colour("#FF0000"));
 
-    // The 'view' describes what set of data is drawn
+    let s4 = Scatter::from_vec(expected_max_points.as_slice())
+        .style(scatter::Style::new()
+            .colour("#00FF00"));
+
     let v = View::new()
         .add(&s1)
         .add(&s2)
         .add(&s3)
+        .add(&s4)
         .x_range(0., max_hw)
         .y_range(0., max_freq)
         .x_label("Hamming Weight Reduction")
         .y_label("Frequency");
 
-    // A page with a single view is then saved to an SVG file
     Page::single(&v).save(output_filename);
 
     println!("Done");
@@ -227,7 +252,7 @@ fn plot_points(output_filename: &str, n: usize, deltas: Vec<(usize, i64)>, expec
     println!("Done");
 }
 
-fn generate_reductions(n: usize, h: usize) -> (Vec<i64>, i64, i64) {
+fn generate_reductions(n: usize, h: usize) -> (Vec<i64>, i64, i64, i64) {
     let (f, g) = randomly_generate_message(n, h);
 
     let mut pub_key = f.clone();
@@ -256,11 +281,15 @@ fn generate_reductions(n: usize, h: usize) -> (Vec<i64>, i64, i64) {
         .filter(|(i, x)| !expected_bits.contains(i))
         .fold(i64::MIN, |acc, (_, x)| acc.max(*x));
 
+    let expected_points_min_hamming_weight_reduction = deltas.iter()
+        .filter(|(i, x)| expected_bits.contains(i))
+        .fold(i64::MAX, |acc, (_, x)| acc.min(*x));
+
     let expected_points_max_hamming_weight_reduction = deltas.iter()
         .filter(|(i, x)| expected_bits.contains(i))
         .fold(i64::MIN, |acc, (_, x)| acc.max(*x));
 
-    (expected_point_hamming_weight_reductions, unexpected_points_max_hamming_weight_reduction, expected_points_max_hamming_weight_reduction)
+    (expected_point_hamming_weight_reductions, unexpected_points_max_hamming_weight_reduction, expected_points_min_hamming_weight_reduction, expected_points_max_hamming_weight_reduction)
 }
 
 fn delta(minuend: &BitField, subtrahend: &BitField, shift_amt: usize) -> i64 {
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,8 @@
 use mersennepkc::*;
 
 fn main() {
-    let n = 9689;
-    let h = 32;
+    let n = 86243;
+    let h = 128;
 
     let (f, g) = randomly_generate_message(n, h);
     println!("F, G: {}, {}", f.to_string(), g.to_string());
